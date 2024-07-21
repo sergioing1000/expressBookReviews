6e9a8e9a2ee768414798e79ca5049f48e30e14da
Sergio Cruz <sergioing@yahoo.com>
2024-07-21 07:41:52 -0500

    new file:   Screenshoots/1-getallbooks.png 	new file:   Screenshoots/2-gedetailsISBN.png 	new file:   Screenshoots/3-getbooksbyathor.png 	new file:   Screenshoots/4-getbooksbytitle.png 	new file:   Screenshoots/6-register.png 	new file:   Screenshoots/7-login.png 	modified:   final_project/router/auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,32 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const userIsValid = (username)=>{ //returns boolean
+    //write code to check is the username is valid
+
+    // The username to check
+    const usernameToCheck = username;
+
+    // Check if the username exists in the array
+    const userExists = users.some(user => user.username === usernameToCheck);
+
+    if (userExists) {
+        console.log(`User ${usernameToCheck} exists.`);
+        return true;
+
+    } else {
+        console.log(`User ${usernameToCheck} does not exist.`);
+        return false;
+    }
+}
+
+const findBookByISBN = function (isbnToSearch) {
+    for (let key in books) {
+      if (books[key]["ISBN-10"] === isbnToSearch) {
+          return books[key];
+      }
+    }
+    return null;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
@@ -55,11 +79,73 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+
+    const username = req.body.username;
+    const review = req.body.review;
+    const isbn = req.params.isbn;
     
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented -- add review"});
+  console.log("The username is : " + username);
+  console.log("the revbiew is: " + review);
+  console.log("The ISBN is: " + isbn);
+
+  function addReview(isbn, review, username) {
+    for (let key in books) {
+        if (books[key]["ISBN-10"] === isbn) {
+            let reviewId = Object.keys(books[key].reviews).length + 1;
+
+                let reviewFound = false;
+
+                for (let id in books[key].reviews) {
+                    if (books[key].reviews[id].username === username) {
+                        books[key].reviews[id].review = review;
+                        reviewFound = true;
+                        break;
+                    }
+                }
+
+                if (!reviewFound) {
+                    const newId = String(Object.keys(books[key].reviews).length + 1);
+                    books[key].reviews[newId] = {
+                        "username": username,
+                        "review": review
+                    };
+                }
+                //books[key].reviews[reviewId] = {username, review};
+
+            return `Review added to book with ISBN ${isbn}`;
+        }
+    }
+    return `Book with ISBN ${isbn} not found`;
+  }
+
+  if (userIsValid(username)){
+
+    if (findBookByISBN(isbn)){
+
+        console.log(addReview(isbn, review, username));
+
+        console.log(books);
+
+        return res.status(302).json({message: `the ${username} and the ISBN ${isbn } are valid and have been fopund`});
+      }
+      else{
+        return res.status(206).json({message: `the ${username} is valid but the ISBN ${isbn} is NOT VALID` });
+      }
+  }
+  else{
+    return res.status(404).json({message: `Inavlid username.`})
+  }
+  
+});
+
+// Delete a book review
+regd_users.delete ( "/auth/review/:isbn", (req, res) => {
+
+
+
 });
 
 module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
+//module.exports.userIsValid = userIsValid;
 module.exports.users = users;
